12284d191bcf93d950b117ed2886bafe22def1f8
bnsilva <biancaneves.ds@gmail.com>
2025-01-21 14:09:16 -0300

    Add Cucumber feature files for issues #107, #108, and #109 with BDD scenarios.

---
+++ features/step_definitions/formulario_steps.rb
@@ -0,0 +1,23 @@
+Given('Eu sou um usuário autenticado') do
+  pending
+end
+
+Given('Eu estou matriculado em turmas com formulários pendentes') do
+  pending
+end
+
+When('Eu acesso a página de formulários pendentes') do
+  pending
+end
+
+Then('Eu devo ver a lista de formulários não respondidos') do
+  pending
+end
+
+Given('Eu estou matriculado em turmas sem formulários pendentes') do
+  pending
+end
+
+Then('Deve exibir uma mensagem {string}') do |mensagem|
+  pending
+end
