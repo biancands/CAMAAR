489d38ccacc1a08cefc36f79d0c660b1bc9a2c6c
Nilo Silva <danilo.silveira.silv@gmail.com>
2025-01-21 21:32:49 -0300

    Add Cucumber feature files for issues EngSwCIC#113, EngSwCIC#112, EngSwCIC#111 and EngSwCIC#110

---
+++ features/step_definitions/admin_steps.rb
@@ -0,0 +1,65 @@
+Given('Eu sou um administrador autenticado') do
+    @administrador_autenticado = true
+    puts "Administrador autenticado: #{@administrador_autenticado}"
+    pending
+  end
+  
+  Given('Existe pelo menos uma turma cadastrada no sistema') do
+    @turmas = [{ id: 1, nome: 'Turma A' }]
+    puts "Turmas cadastradas: #{@turmas.map { |t| t[:nome] }.join(', ')}"
+    pending
+  end
+  
+  When('Eu acesso a página de criação de formulários') do
+    @pagina_acessada = 'criação de formulários'
+    puts "Página acessada: #{@pagina_acessada}"
+    pending
+  end
+  
+  When('Eu seleciono a opção {string}') do |opcao|
+    @opcao_selecionada = opcao
+    puts "Opção selecionada: #{@opcao_selecionada}"
+    pending
+  end
+  
+  When('Eu preencho as informações do formulário') do
+    @formulario = { titulo: 'Avaliação de Desempenho', perguntas: ['Pergunta 1', 'Pergunta 2'] }
+    puts "Formulário preenchido: #{@formulario[:titulo]}"
+    pending
+  end
+  
+  When('Eu associo o formulário a uma turma específica') do
+    @formulario[:turma] = @turmas.first
+    puts "Formulário associado à turma: #{@formulario[:turma][:nome]}"
+    pending
+  end
+  
+  When('Eu salvo o formulário') do
+    if @opcao_selecionada.nil? || @formulario[:titulo].nil? || @formulario[:perguntas].empty?
+      @formulario_salvo = false
+      puts "Erro ao salvar o formulário: campos obrigatórios ausentes."
+    else
+      @formulario_salvo = true
+      puts "Formulário salvo com sucesso."
+    end
+    pending
+  end
+  
+  Then('O formulário deve ser criado e associado aos {string} da turma selecionada') do |publico_alvo|
+    if @formulario_salvo && @opcao_selecionada == publico_alvo
+      puts "Formulário criado com sucesso para os #{publico_alvo}."
+    else
+      puts "Falha ao criar o formulário."
+    end
+    pending
+  end
+  
+  Then('Deve exibir uma mensagem {string}') do |mensagem|
+    if @formulario_salvo
+      puts "Mensagem de sucesso: #{mensagem}"
+    else
+      puts "Mensagem de erro: #{mensagem}"
+    end
+    pending
+  end
+  
