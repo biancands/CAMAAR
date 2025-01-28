c7d64fcdcfb760222180f9580e8d184fe8811d79
Nilo Silva <danilo.silveira.silv@gmail.com>
2025-01-28 20:06:35 -0300

    :lipstick: animação de transcição

--- app/javascript/pages/home_index.js
+++ app/javascript/pages/home_index.js
@@ -1,1 +1,13 @@
-console.log('home_index.js carregado com sucesso');
+var btnSignin = document.querySelector("#signin");
+var btnSignup = document.querySelector("#signup");
+
+var body = document.querySelector("body");
+
+
+btnSignin.addEventListener("click", function () {
+   body.className = "sign-in-js"; 
+});
+
+btnSignup.addEventListener("click", function () {
+    body.className = "sign-up-js";
+})
