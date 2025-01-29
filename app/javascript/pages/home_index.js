87ecf615d85a8d73e27f44519104f58209ff0760
Nilo Silva <danilo.silveira.silv@gmail.com>
2025-01-29 10:35:26 -0300

    :sparkles: forms de recuperação de senha

--- app/javascript/pages/home_index.js
+++ app/javascript/pages/home_index.js
@@ -1,13 +1,17 @@
 var btnSignin = document.querySelector("#signin");
 var btnSignup = document.querySelector("#signup");
+var forgotPasswordLink = document.querySelector("#forgetspassword");
 
 var body = document.querySelector("body");
 
-
 btnSignin.addEventListener("click", function () {
-   body.className = "sign-in-js"; 
+  body.className = "sign-in-js";
 });
 
 btnSignup.addEventListener("click", function () {
-    body.className = "sign-up-js";
-})
+  body.className = "sign-up-js";
+});
+
+forgotPasswordLink.addEventListener("click", function () {
+  body.className = "reset-password-js";
+});
