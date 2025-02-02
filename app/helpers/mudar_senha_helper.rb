125a9ff228b394a273f171ee37e01cd2fc04b07e
Nilo Silva <danilo.silveira.silv@gmail.com>
2025-02-02 18:56:32 -0300

    :sparkles: tela de mudança de senha e alteração dos titulos de algumas paginas

---
+++ app/helpers/mudar_senha_helper.rb
@@ -0,0 +1,2 @@
+module MudarSenhaHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'resultados', to: 'resultados#show'
   get 'templates', to: 'templates#show'
   get 'enviar_forms', to: 'enviar_forms#show'
+  get 'mudar_senha', to: 'mudar_senha#show'
   get "/forms/:id", to: "forms#show"
 end
