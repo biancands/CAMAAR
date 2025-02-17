7fcd8b5d38f0b7b3a6737fafb7d9f81365496844
Nilo Silva <danilo.silveira.silv@gmail.com>
2025-02-17 13:21:17 -0300

    Melhora a documentação no arquivo application.rb

--- db/migrate/20250201232331_add_curso_to_usuarios.rb
+++ db/migrate/20250201232331_add_curso_to_usuarios.rb
@@ -1,3 +1,6 @@
+# Adiciona a coluna `curso` à tabela `usuarios`.
+#
+# Esta migração inclui um novo campo para armazenar o curso associado a um usuário.
 class AddCursoToUsuarios < ActiveRecord::Migration[8.0]
   def change
     add_column :usuarios, :curso, :string
